de63d173f093718cdafdd198d717095f6ae049be
Marco Meyer-Conde <marco@tcu.ac.jp>
2026-07-07 13:20:42 +0900

    Fill in real source, license, and build steps for eMELA 1.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,8 @@
 #! /usr/bin/bash
 set -e
 
-mkdir -p build
-cd build
-
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_BUILD_TYPE=Release
+cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DWITH_LHAPDF=ON -S source -B build
 
 NPROC=$(nproc 2>/dev/null || sysctl -n hw.ncpu)
-make -j"$NPROC"
-make install
+cmake --build build --parallel="${NPROC}"
+cmake --install build
